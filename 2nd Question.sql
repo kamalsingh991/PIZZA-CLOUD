531c40a3fbf7ed021e7fe0ac37247dd63a0f16b4
Kamal Singh <goldgolu991@gmail.com>
2025-02-15 15:28:50 +0530

    Delete 2nd Question.sql

--- 2nd Question.sql
+++
@@ -1,9 +0,0 @@
--- 2. Calculate the total revenue generated from pizza sales.
-
-SELECT 
-    ROUND(SUM(pizzas.price * order_details.quantity),
-            2) AS Total_Revenue
-FROM
-    pizzas
-        JOIN
-    order_details ON pizzas.pizza_id = order_details.pizza_id;
