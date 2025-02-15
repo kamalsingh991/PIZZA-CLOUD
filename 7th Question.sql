a7c043c6fd04ebfc86e61d087d1c9980dc51e086
Kamal Singh <goldgolu991@gmail.com>
2025-02-15 15:28:04 +0530

    Delete 7th Question.sql

--- 7th Question.sql
+++
@@ -1,7 +0,0 @@
--- 7. Determine the distribution of orders by hour of the day.
-
-SELECT 
-    HOUR(order_time) AS hour, COUNT(order_id) AS Orders_Count
-FROM
-    orders
-GROUP BY HOUR(orders.order_time)
